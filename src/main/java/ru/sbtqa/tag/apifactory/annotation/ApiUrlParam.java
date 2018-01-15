62202cef087e614598609c1c630bf8f48d6140ac
0I0u3uk <svetlakovanton@gmail.com>
2018-01-15 13:58:34 +0700

    CodeReview: Delete comment

--- src/main/java/ru/sbtqa/tag/apifactory/annotation/ApiUrlParam.java
+++ src/main/java/ru/sbtqa/tag/apifactory/annotation/ApiUrlParam.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by sbt-svetlakov-av on 15.01.18.
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ApiUrlParam {
